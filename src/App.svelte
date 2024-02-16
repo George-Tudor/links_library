d306e0d954022f47ae3a021f23b18b091ff724ef
George Tudor <Alexander.George.Tudor@gmail.com>
2024-02-16 16:07:40 -0600

    Link item, link grid, and header components created, dummy data for links added

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,83 @@
 <script>
+import Header from "./UI/Header.svelte";
+import LinkItem from "./Links/LinkItem.svelte";
+import LinkGrid from "./Links/LinkGrid.svelte";
+
+let title = "";
+let description = "";
+let url = "";
+let imageUrl = "";
+let userNotes= "";
+let tags = [];
+
+let links = [
+    {
+        id: 'l1',
+        title: 'Marvel.com',
+        url: 'https://www.marvel.com/',
+        description: 'Marvel.com is the official site of Marvel Entertainment! Browse official Marvel movies, characters, comics, TV shows, videos, & more.',
+        imageUrl: 'https://logos-world.net/wp-content/uploads/2020/11/Marvel-Logo-2000-2012.png',
+        userNotes: 'Website for the best comic info',
+        tags: ['comics', 'superheros', 'spiderman', 'wolverine']
+    },
+    {
+        id: 'l2',
+        title: 'dc.com',
+        url: 'https://www.dc.com/comics',
+        description: 'DC.com - Welcome to the Official Site for DC. DC is home to the world\'s Greatest Super Heroes, including SUPERMAN, BATMAN, WONDER WOMAN, GREEN LANTERN, THE FLASH, AQUAMAN and more',
+        imageUrl: 'https://www.dc.com/img/dc-logo.svg',
+        userNotes: 'Website for the 2nd best comic info',
+        tags: ['comics', 'superheros', 'superman', 'batman']
+    },
+    {
+        id: 'l3',
+        title: 'NASA',
+        url: 'https://www.nasa.gov/',
+        description: 'NASA.gov brings you the latest news, images and videos from America\'s space agency, pioneering the future in space exploration, scientific discovery and aeronautics research.',
+        imageUrl: 'https://www.nasa.gov/wp-content/themes/nasa/assets/images/nasa-logo.svg',
+        userNotes: '',
+        tags: ['space', 'astronomy', 'galaxies', 'solar system']
+    },
+    {
+        id: 'l4',
+        title: 'National Geographic',
+        url: 'https://www.nationalgeographic.com/',
+        description: 'Explore National Geographic. A world leader in geography, cartography and exploration.',
+        imageUrl: 'https://logos-world.net/wp-content/uploads/2020/09/National-Geographic-Symbol.jpg',
+        userNotes: 'Geography website with info from around',
+        tags: ['lions', 'tigers', 'bears', 'oh my']
+    },
+    {
+        id: 'l5',
+        title: 'Image Comics',
+        url: 'https://imagecomics.com/',
+        description: 'Image is a comics and graphic novels publisher formed in 1992 by 7 of the comics industry\'s best-selling artists, and is the 3rd largest comics publisher in the United States.',
+        imageUrl: 'https://www.cuartomundo.cl/wp-content/uploads/2015/09/Image-Comics-Logo-400x200.png',
+        userNotes: 'Comic website for Spawn and other characters',
+        tags: ['comics', 'superheros', 'spawn', 'transformers']
+    },
+    {
+        id: 'l6',
+        title: 'McFarlane',
+        url: 'https://mcfarlane.com/',
+        description: 'McFarlane.com :: The home all things Todd McFarlane',
+        imageUrl: 'https://mcfarlane.com/wp-content/themes/mcfarlane/img/header-mcFarlane-logo.jpg',
+        userNotes: 'Website of Todd McFarlange, creator of Spawn',
+        tags: ['comics', 'superheros', 'spawn']
+    }
+]
 
 </script>
 
 <main>
 
+    <Header />
+    <LinkGrid {links}/>
+
 </main>
 
 <style>
-
+    main {
+        margin-top: 5rem;
+    }
 </style>
---
+++ src/Links/LinkGrid.svelte
@@ -0,0 +1,34 @@
+<script>
+
+    import LinkItem from "./LinkItem.svelte";
+
+    export let links;
+</script>
+
+<section id="links">
+    {#each links as link}
+        <LinkItem
+                title={link.title}
+                description={link.description}
+                url={link.url}
+                imageUrl={link.imageUrl}
+                notes={link.notes}
+                tags={link.tags}
+            />
+    {/each}
+</section>
+
+<style>
+    section {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-gap: 1rem;
+    }
+
+    @media (min-width: 768px) {
+        section {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+</style>
